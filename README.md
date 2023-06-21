bc5cd1283b93d7b7e4ed57c0cb10add0298059fa
Max Voloshinskii <me@voloshinskii.ru>
2023-06-21 04:31:05 +0400

    add script

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # localize-ai
-Localize your services with hustle 🕺
+Localize your services without pain 🕺
+
+### Usage
+
+Create workflow file in your repository, for example `workflow.yaml`:
+
+```yaml
+on: [push]
+name: Localize strings
+on: [push]
+jobs:
+  localize:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Localize strings
+        env:
+          OPENAI_TOKEN: ${{ secrets.API_TOKEN }}
+          LOCALES_PATH: ../locales
+          MODEL: gpt-4
+          MAIN_LOCALE: en
+        uses: voloshinskii/localize-ai@v1
+
+      - name: commit and push all changed files
+        env:
+          CI_COMMIT_MESSAGE: Localize new strings
+          CI_COMMIT_AUTHOR: CI
+        run: |
+          git config --global user.name "${{ env.CI_COMMIT_AUTHOR }}"
+          git config --global user.email "username@users.noreply.github.com"
+          git commit -a -m "${{ env.CI_COMMIT_MESSAGE }}"
+          git push
+```
+
+### Known limitations
+    - Only supports .json
+    - Pluralization is not supported now
---
+++ json.js
@@ -0,0 +1,103 @@
+const fs = require('fs');
+const path = require('path');
+const { Configuration, OpenAIApi } = require("openai");
+
+const apiKey = process.env.OPENAI_TOKEN;
+const mainLocale = process.env.MAIN_LOCALE || 'en';
+const model = process.env.MODEL || 'gpt-4';
+const dir = path.join(path.dirname(require.main.filename), process.env.LOCALES_PATH || './locales');
+
+const dropPluralRegex = /(_one|_two|_few|_many|_other|_zero)$/;
+
+const configuration = new Configuration({
+    apiKey,
+});
+const openai = new OpenAIApi(configuration);
+
+const locales = {};
+fs.readdirSync(dir).forEach(file => {
+    const locale = file.split('.')[0];
+    locales[locale] = require(path.join(dir, file));
+});
+
+function asyncForEach(array, cb) {
+    return new Promise((resolve, reject) => {
+        (async function loop() {
+            for (let index = 0; index < array.length; index++) {
+                await cb(array[index], index, array);
+            }
+            resolve();
+        })();
+    });
+}
+
+function deepDiff(a, b) {
+    const diff = {};
+    Object.entries(a).forEach(([key, value]) => {
+        if (key.match(dropPluralRegex)) {
+            return;
+        }
+        if (typeof value === 'string') {
+            if (!b[key]) {
+                diff[key] = value;
+            }
+        } else {
+            diff[key] = deepDiff(value, b[key] || {});
+        }
+    });
+    return diff;
+}
+
+async function getTranslationOfStringFromBackend(string, locale) {
+    const prompt = `Translate the following string to ${locale}:\n\n${string}\n\n`;
+    const messages = [
+        { "role": "user", "content": prompt },
+    ];
+
+    const completion = await openai.createChatCompletion({
+        model,
+        messages,
+        max_tokens: 2000,
+    });
+
+    return completion.data.choices[0]?.message.content;
+}
+
+function deepCopy(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
+async function deepLocalization(locale, diff, locale_key) {
+    const loc = deepCopy(locale);
+    await asyncForEach(Object.entries(diff), async ([key, value]) => {
+        if (typeof value === 'string') {
+            loc[key] = await getTranslationOfStringFromBackend(value, locale_key);
+        } else {
+            loc[key] = await deepLocalization(loc[key] || {}, value, locale_key);
+        }
+    });
+    return loc;
+}
+
+function saveAsJson() {
+    Object.entries(locales).forEach(([locale, translations]) => {
+        fs.writeFileSync(path.join(dir, `${locale}.json`), JSON.stringify(translations, null, 2));
+    });
+}
+
+async function localizeRestLanguages() {
+    await asyncForEach(Object.entries(locales), async ([locale, translations]) => {
+        if (locale === mainLocale) {
+            return;
+        }
+        console.log(`Localizing ${locale}...`);
+        const diff = deepDiff(locales[mainLocale], translations);
+        console.log('detected new strings: ');
+        console.log(JSON.stringify(diff, null, 2));
+
+        locales[locale] = await deepLocalization(locales[locale], diff, locale);
+    });
+    saveAsJson();
+}
+
+localizeRestLanguages();
